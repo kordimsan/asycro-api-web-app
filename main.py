029f6f1d33d7977c7bb53a7e35fca674b6a49322
Dmitry Korzunin <kordimsan@mail.ru>
2021-05-31 23:03:26 +0500

    add validation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, BackgroundTasks
+from fastapi import FastAPI, BackgroundTasks, Path
 import asyncio
 from threading import Thread, Lock
 
@@ -10,7 +10,11 @@ worker = Worker()
 Thread(target=worker.run_worker).start()
 
 @app.get("/add")
-async def add_item(background_tasks: BackgroundTasks, num: int, timeout: int = 5):
+async def add_item(
+    background_tasks: BackgroundTasks, 
+    num: int = Path(..., title="The num might be positive integer", ge=0, le=1000), 
+    timeout: int = Path(5, title="The num might be in seconds", ge=0, le=1000)
+):
     background_tasks.add_task(worker.add_task, num, timeout)
     return {"message": "task has added"}
 
